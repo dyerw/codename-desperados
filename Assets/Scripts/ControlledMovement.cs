3db5baf3876098fb8c2890709cb86afcb748fe16
dyerw <liamd94@gmail.com>
2020-04-08 17:39:56 -0500

    Rendering and basic networking/movement

---
+++ Assets/Scripts/ControlledMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ControlledMovement : MonoBehaviour
+{
+
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float speed;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpSpeed;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            moveDirection = transform.right * x + transform.forward * z;
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Rendering;
+
+/**
+ * Handles disabling/enabling things on spawn
+ */
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] Behaviour[] componentsToDisable;
+    [SerializeField] Camera myCamera;
+    [SerializeField] GameObject firstPersonView;
+    [SerializeField] GameObject thirdPersonView;
+    void Start()
+    {
+        Renderer[] thirdPersonRenderers = thirdPersonView.GetComponentsInChildren<Renderer>();
+        Renderer[] firstPersonRenderers = firstPersonView.GetComponentsInChildren<Renderer>();
+        // If this Player instance is NOT the local player disable all the linked stuff
+        if (!isLocalPlayer)
+        {
+            foreach(Renderer renderer in firstPersonRenderers)
+            {
+                renderer.enabled = false;
+            }
+            foreach (Behaviour b in componentsToDisable)
+            {
+                b.enabled = false;
+            }
+        } else
+        {
+            foreach (Renderer renderer in thirdPersonRenderers)
+            {
+                renderer.shadowCastingMode = ShadowCastingMode.ShadowsOnly;
+            }
+            foreach (Renderer renderer in firstPersonRenderers)
+            {
+                renderer.shadowCastingMode = ShadowCastingMode.Off;
+            }
+            // Disable all cameras that aren't the first person camera for this player
+            Camera[] allCameras = Camera.allCameras;
+            foreach (Camera c in allCameras)
+            {
+                if (c != myCamera)
+                {
+                    c.enabled = false;
+                }
+            }
+
+
+        }
+
+    }
+
+}
